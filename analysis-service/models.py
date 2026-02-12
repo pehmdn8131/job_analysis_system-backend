8e8b4eb88819eb8394f55f8513b90abffc66d4f2
pehmdn8131 <126678693+pehmdn8131@users.noreply.github.com>
2026-02-12 08:34:45 +0800

    Delete analysis-service directory

--- analysis-service/models.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-class Job(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    salary_min = db.Column(db.Integer)
-    city = db.Column(db.String(50))
